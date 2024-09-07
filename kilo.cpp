27d1f3d20f213b745d7f9ff55a6834c7246e73ff
pharooque <32064752+pharooque@users.noreply.github.com>
2024-09-07 10:26:34 +0100

    Enabled raw mode

--- kilo.cpp
+++ kilo.cpp
@@ -1,13 +1,22 @@
+#include <windows.h>
 #include <iostream>
+
+void enableRawMode()
+{
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD raw{};
+
+    GetConsoleMode(hStdin, &raw);       // Get curremt console mode
+    SetConsoleMode(hStdin, raw & ~ENABLE_ECHO_INPUT);   //Disable echo
+}
 
 int main(int argc, char const *argv[])
 {
+    enableRawMode();
     char c{};
-
-    while (std::cin.get(c))
+    while (std::cin.get(c) && c != 'q')
     {
-        /* code */
+        /* Keep looping until 'q' is pressed */
     }
-    
     return 0;
 }
